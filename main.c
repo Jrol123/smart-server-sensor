73b82223cd5ca7d1a75709d0244cdd4ff21af172
Artemii <angap4@gmail.com>
2024-06-19 13:40:07 +1000

    Попытка сделать получение данных с подвязкой
    Всё ещё выдаёт 8192

--- main.c
+++ main.c
@@ -8,22 +8,24 @@ int main(void)
     ds18_params_t params_datchik;
 
     params_datchik.in_mode = GPIO_IN;
-    params_datchik.out_mode = GPIO_OUT;
+    params_datchik.out_mode = GPIO_OD_PU;
     params_datchik.pin = GPIO_PIN(PORT_A, 0);
 
     ds18_t datchik;
     // datchik.params = params_datchik;
 
-    ds18_init(&datchik, &params_datchik);
+    int status = ds18_init(&datchik, &params_datchik);
+
+    printf("status of init: %i\n", status);
 
     while (1)
     {
         int16_t temp;
-        ds18_read(&datchik, &temp);
+        ds18_get_temperature(&datchik, &temp);
         // temp /= 100;
         // puts("read");
 
-        printf("output: %d\n", temp);
+        printf("output: %i\n", temp);
         xtimer_sleep(1);
     }
 }
