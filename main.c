be0918741ece1be29c5a3cae16a6fc2e5b062080
Artemii <angap4@gmail.com>
2024-06-19 12:12:25 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include"ds18.h"
+#include "ds18.h"
+#include "xtimer.h"
 #include "periph/gpio.h"
 #include "board.h"
-
 
 int main(void)
 {
@@ -15,4 +15,15 @@ int main(void)
     // datchik.params = params_datchik;
 
     ds18_init(&datchik, &params_datchik);
+
+    while (1)
+    {
+        int16_t temp;
+        ds18_read(&datchik, &temp);
+        // temp /= 100;
+        // puts("read");
+
+        printf("output: %d\n", temp);
+        xtimer_sleep(1);
+    }
 }
