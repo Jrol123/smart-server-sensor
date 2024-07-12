5ce33ac70f704961e694b19b59b62c36d6d59fc3
Tyferse <qwerty482803@gmail.com>
2024-07-12 13:25:13 +1000

    da

--- server_visual/app.py
+++ server_visual/app.py
@@ -6,10 +6,8 @@ from read_data import read
 import time
 from threading import Thread
 
-import numpy as np
-
 # Метки оси x для отображения данных
-x = np.arange(0, 10, 0.1)
+x = list(range(10))
 
 # Интервал получения данных с датчиков
 SLEEP_TIME = 3
@@ -81,11 +79,11 @@ def update_data():
         data = Record.query.order_by(Record.timestamp.desc()).all()
         time_arr = [record.timestamp for record in data]
         temp_arr = [record.temperature for record in data]
-        y = [record.temperature for record in Record.query.order_by(Record.timestamp.desc()).limit(100).all()[::-1]]
+        y = [record.temperature for record in Record.query.order_by(Record.timestamp.desc()).limit(10).all()[::-1]]
         connected_mk = Record.query.order_by(Record.timestamp.desc()).first().count_controllers
 
         # Преобразование чисел в формат Python-friendly для сериализации в JSON
-        data = {'x': x.tolist(), 'y': y, 'cmk': connected_mk, 'time': time_arr, 'temp': temp_arr}
+        data = {'x': x, 'y': y, 'cmk': connected_mk, 'time': time_arr, 'temp': temp_arr}
 
         return jsonify(data)
 
